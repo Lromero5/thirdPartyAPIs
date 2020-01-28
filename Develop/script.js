3af78d120057e81b3f747adfb108b04f3e6860a9
LRomero <ldnavar5@gmail.com>
2020-01-27 19:54:50 -0600

    added html, css, and script files

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+//display current date
+
+
+
+
+// must create a way for the table to be updated dynamically using jquery.
+
+
+
+//standard business hours should be 9 a.m. to 5 p.m.
+//the left side should have the hour 
+//Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day.
+
+
+
+
+//create an html element that adds a field to the middle where the user can input data. 
+
+
+// Create a save button that will saved to local storage.
+
+//
